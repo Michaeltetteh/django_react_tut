fdf49bfd7c858730ae795e6ffa74a54565c025ab
michaeltetteh <miketetteh751@outlook.com>
2020-03-26 12:24:35 +0000

    createuser view,serializer

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import permissions
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from .serializers import MyTokenObtainPairSerializer,CustomUserSerializer
 
-# Create your views here.
+class ObtainTokenPairWithColorView(TokenObtainPairView):
+    # permission_classes = (permissions.AllowAny,)
+    serializer_class = MyTokenObtainPairSerializer
+
+class CustomUserCreate(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format='json'):
+        serializer = CustomUserSerializer(data=request.data)
+        # print(serializer)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                json = serializer.data
+                return Response(json,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  
