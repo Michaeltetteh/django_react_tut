e19e55ff8e5b2b91a9609d8b44c6c5b87c7ec436
michaeltetteh <miketetteh751@outlook.com>
2020-03-28 12:29:22 +0000

    react routing and links setup

--- src/frontend/src/components/App.js
+++ src/frontend/src/components/App.js
@@ -1,26 +1,31 @@
-// import React, { Component} from "react";
-
-// class App extends Components{
-//     render(){
-//         return(
-//             <div className="site">
-//                 <h1>
-//                     You have no power here!.
-//                 </h1>
-//             </div>
-//         );
-//     }
-// }
-
-// export default App;
-
 import React, {Component} from "react";
+import {Switch,Route,Link} from "react-router-dom";
+import Login from "./login";
+import Signup from "./signup";
 
 class App extends Component {
     render() {
         return ( 
             <div className = "site" >
-                <h1 > You have no power here!.</h1> 
+                <nav>
+                    <Link className={"nav-link"} to={"/"}>
+                        Home
+                    </Link>
+                    <Link className={"nav-link"} to={"/login/"}>
+                        Login
+                    </Link>
+                    <Link className={"nav-link"} to={"/signup/"}>
+                        Signup
+                    </Link>
+                </nav>
+                <main>
+                    <h1 > You have no power here!.</h1> 
+                    <Switch>
+                        <Route exact path={"/login/"} component={Login}/>
+                        <Route exact path={'/signup/'} component={Signup}/>
+                        <Route exact path={"/"} render={() => <div> Home Again</div>}/>
+                    </Switch>
+                </main>
             </div>
         );
     }
