29968dfbd6c877a023f36a4207f3c8c4b6d3a1c9
michaeltetteh <miketetteh751@outlook.com>
2020-03-28 10:58:18 +0000

    react frontend working

---
+++ src/frontend/src/components/App.js
@@ -0,0 +1,29 @@
+// import React, { Component} from "react";
+
+// class App extends Components{
+//     render(){
+//         return(
+//             <div className="site">
+//                 <h1>
+//                     You have no power here!.
+//                 </h1>
+//             </div>
+//         );
+//     }
+// }
+
+// export default App;
+
+import React, {Component} from "react";
+
+class App extends Component {
+    render() {
+        return ( 
+            <div className = "site" >
+                <h1 > You have no power here!.</h1> 
+            </div>
+        );
+    }
+}
+
+export default App;
