1fe5881f98547767c77af652d900983ecd519897
michaeltetteh <miketetteh751@outlook.com>
2020-03-28 14:28:16 +0000

    setting up axios

--- src/frontend/src/components/signup.js
+++ src/frontend/src/components/signup.js
@@ -3,12 +3,45 @@ import React, {Component} from 'react';
 class Signup extends Component{
     constructor(props){
         super(props);
+        this.state = {
+            Username: "",
+            password: "",
+            email: ""
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
+
+    handleChange(evennt){
+        this.setState({[event.target.name]: event.target.value});
+    }
+
+    handleSubmit(event){
+        alert('A username and password was submitted: ' + this.state.username + " " + 
+        this.state.password + " " + this.state.email);
+        event.preventDefault();
+    }
+
 
     render() {
         return(
-            <div>
-                <h2>Signup page</h2>
+            <div>Signup
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Username:
+                        <input name="username" type="text" value={this.state.username} onChange={this.handleChange}/>
+                    </label>
+                    <label>
+                        Email:
+                        <input name="email" type="email" value={this.state.email} onChange={this.handleChange}/>
+                    </label>
+                    <label>
+                        Password
+                        <input name="password" type="password" value={this.state.password} onChange={this.handleChange}/>
+                    </label>
+                    <input type="submit" value="Submit"/>
+                </form>
             </div>
         )
     }
